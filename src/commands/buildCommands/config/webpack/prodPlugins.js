d3ae1336b3a262961902aeb2ba926911de89e991
Michael McGahan <mike.mcgahan@gmail.com>
2017-06-21 14:54:52 +1200

    sw-precache-plugin

--- src/commands/buildCommands/config/webpack/prodPlugins.js
+++ src/commands/buildCommands/config/webpack/prodPlugins.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 
 module.exports = [
 	// Tells loaders to optimize what they can since in minimize mode
@@ -15,5 +16,12 @@ module.exports = [
 		output: {
 			comments: false,
 		},
+	}),
+	new SWPrecacheWebpackPlugin({
+		cacheId: 'mwp',
+		dontCacheBustUrlsMatching: /\.\w{8}\./,
+		filename: 'asset-service-worker.js',
+		minify: true,
+		staticFileGlobsIgnorePatterns: [/\.map$/, /.json$/],
 	}),
 ];
