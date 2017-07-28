48c3764c3dbb1f7f49e71f7da9343b5aef1ced36
Matt Kime <matt@mattki.me>
2017-07-28 11:28:40 -0500

    remove mistaken commit

--- src/commands/txCommands/pushNewContent.js
+++ src/commands/txCommands/pushNewContent.js
@@ -75,14 +75,12 @@ const masterAndResourceTrns$ = Rx.Observable
 
 // don't run against master! will delete trn content on transifex
 const branchCheck$ = gitBranch$.do(branchName => {
-	/*
 	if (branchName === 'master') {
 		console.log(
 			'do not run this script on master. it will kill the master resource on Transifex.'
 		);
 		process.exit(0);
 	}
-	*/
 });
 
 const pushContent$ = Rx.Observable
