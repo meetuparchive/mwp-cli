585dbcb92b617ece4dadcc9f679a2b9a06224348
Michael McGahan <mike.mcgahan@gmail.com>
2017-12-30 11:37:56 +1300

    docs plus minor cleanup

--- src/commands/deployUtils/index.js
+++ src/commands/deployUtils/index.js
@@ -7,28 +7,20 @@ const cloudApi = require('./cloudApi');
 const getApi = config => {
 	const { auth, appsId, servicesId } = config;
 
+	/*
+   * Get the traffic split among deployed versions
+   * https://cloud.google.com/appengine/docs/admin-api/reference/rest/v1/apps.services#TrafficSplit
+   */
 	const allocations = () =>
 		cloudApi.services
 			.get({ auth, appsId, servicesId })
 			.then(({ split: { allocations } }) => allocations);
 	const operations = getOperations(config);
 	const versions = getVersions(config, { allocations, operations });
-	const migrate = getMigrate(config, { allocations, operations, versions });
 	const deploy = getDeploy(config, { operations, versions });
+	const migrate = getMigrate(config, { allocations, operations, versions });
 
-	/*
-   * Get the traffic split among deployed versions
-   * https://cloud.google.com/appengine/docs/admin-api/reference/rest/v1/apps.services#TrafficSplit
-   */
-	const _api = {
-		deploy,
-		versions,
-		migrate,
-	};
-
-	return _api;
+	return { deploy, migrate, versions };
 };
 
-module.exports = {
-	getApi,
-};
+module.exports = { getApi };
