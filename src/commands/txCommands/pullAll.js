c66a80680147b988f5aecbde139ab598cc7deb9f
Lori Hutchek <Lori.Hutchek@akqa.com>
2018-01-04 23:56:05 -0500

    adding optional git commit flag

--- src/commands/txCommands/pullAll.js
+++ src/commands/txCommands/pullAll.js
@@ -1,4 +1,5 @@
 const chalk = require('chalk');
+const child_process = require('child_process');
 const Rx = require('rxjs');
 const txlib = require('./util');
 const txPullResource = require('./util/pullResourceTrns');
@@ -8,26 +9,51 @@ const getProjectResourcesList$ =
 		.flatMap(Rx.Observable.from);
 
 
-const getIndividualResource = (resources, iteration) =>
+const getIndividualResource = (resources, iteration, commitOnComplete) => {
+	console.log(chalk.blue(`Starting tx:pull for '${resources[iteration]}'`));
 	txPullResource.pullResourceContent$(resources[iteration]).subscribe(null, null, () => {
-		console.log(`${resources[iteration]} done`);
-		if (iteration+1 < resources.length) {
-			getIndividualResource(resources, iteration+1);
+		console.log(chalk.green(`Completed tx:pull for '${resources[iteration]}'`));
+
+		const doNext = iteration+1 < resources.length;
+
+		if (commitOnComplete) {
+			child_process.exec('git status', (error, stdout, stderr) => {
+				if (stdout.includes('nothing to commit') === false) {
+					child_process.execSync('git add src/trns');
+					child_process.exec(`git commit -m "tx:pull for ${resources[iteration].replace(/-/g, '_')}" --no-verify`, () => {
+						doNext && getIndividualResource(resources, iteration+1, commitOnComplete);
+						console.log(chalk.green(`- commited changes for '${resources[iteration]}'`));
+					});
+				} else {
+					console.log(chalk.yellow(`- no changes to commit for '${resources[iteration]}'`));
+					doNext && getIndividualResource(resources, iteration+1, commitOnComplete);
+				}
+			});
+		} else {
+			doNext && getIndividualResource(resources, iteration+1, commitOnComplete);
 		}
 	});
+};
 
-const getResourceTrns = resources =>
-	getIndividualResource(resources, 0);
+const getResourceTrns = (resources, commitOnComplete) =>
+	getIndividualResource(resources, 0, commitOnComplete);
 
 module.exports = {
 	command: 'pullAll',
 	description: 'pulls all content for resources in from transifex in update date order',
+	builder: yarg =>
+		yarg.option({
+			gitCommit: {
+				alias: 'gh',
+				default: false
+			},
+		}),
 	handler: argv => {
 		txlib.checkEnvVars();
 		console.log(chalk.blue('pulls all content for resources in from transifex in update date order'));
 
 		getProjectResourcesList$
 			.reduce((resources, resource) => [ ...resources, resource ], [])
-			.subscribe(getResourceTrns);
+			.subscribe(resources => getResourceTrns(resources, argv.gitCommit));
 	},
 };
