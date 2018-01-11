1850b5b6b1d7ff0ce4fa3118e0780441f8ec75a8
Lori Hutchek <Lori.Hutchek@akqa.com>
2018-01-11 18:38:37 -0500

    killing note on resource name

--- src/commands/txCommands/util/index.js
+++ src/commands/txCommands/util/index.js
@@ -17,7 +17,7 @@ const TX_PW = process.env.TRANSIFEX_PW;
 const PROJECT = packageConfig.txProject;
 const PROJECT_MASTER = `${PROJECT}-master`; // separate project so translators don't confuse with branch content
 const MASTER_RESOURCE = 'master';
-const ALL_TRANSLATIONS_RESOURCE = 'all_translations'; // #TODO: temp until me and yanyi merge branches
+const ALL_TRANSLATIONS_RESOURCE = 'all_translations';
 
 const tx = new Transifex({
 	project_slug: PROJECT,
