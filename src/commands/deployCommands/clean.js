c90060483b2f335b608a726b9da977cf394bd1a1
Michael McGahan <mike.mcgahan@gmail.com>
2018-02-28 15:02:52 +1300

    organize subcommands

--- src/commands/deployCommands/clean.js
+++ src/commands/deployCommands/clean.js
@@ -1,22 +1,55 @@
+const apiMiddleware = require('../deployUtils/apiMiddleware');
+
+const stopOldVersions = (serving, api, versions) =>
+	Promise.all(
+		api.allocations(),
+		api.versions.instances.running(versions)
+	).then(([allocs, runningVersions]) => {
+		// runningVersions is ordered by start time, so we can find out
+		// where to STOP by adding `serving` to `indexOf` the last item serving traffic
+		const servingIds = Object.keys(allocs);
+		const oldestAllocIndex = runningVersions.reduce(
+			(oldestI, v, i) => (servingIds.includes(v.id) ? i : oldestI),
+			-1
+		);
+		console.log(runningVersions.slice(oldestAllocIndex + serving - 1));
+	});
+// .then(api.versions.stop);
+
+const deleteOldestVersions = (available, api, versions) => {
+	const versionsToDelete = versions.slice(available - 1).map(({ id }) => id);
+	console.log(versionsToDelete);
+	// return api.versions.deleteSafe(versionsToDelete);
+};
+
 module.exports = {
 	command: 'clean',
-	description: 'stop older versions, clean up oldest versions',
+	description: 'stop older versions, delete oldest versions',
 	builder: yargs =>
 		yargs.options({
 			serving: {
 				default: 4,
-				describe: 'Number of versions to keep running',
+				describe: 'Minimum number of versions to keep serving',
 			},
 			available: {
 				default: 25,
 				describe: 'Number of versions to keep available',
 			},
 		}),
-	hander: argv =>
-		argv
-			.getApi()
-			.then(({ deploy, versions, migrate }) => {
-				console.log('okay');
-			})
-			.catch(err => console.error(err)),
+	middlewares: [apiMiddleware],
+	handler: argv => {
+		return argv
+			.getDeployApi()
+			.then(api =>
+				api.versions
+					.get()
+					.then(versions =>
+						Promise.all(
+							stopOldVersions(argv.serving, api, versions),
+							deleteOldestVersions(argv.available, api, versions)
+						)
+					)
+			)
+			.catch(err => console.error(err));
+	},
 };
---
+++ src/commands/deployCommands/create.js
@@ -0,0 +1,87 @@
+const chalk = require('chalk');
+const runE2E = require('../deployUtils/e2e');
+const apiMiddleware = require('../deployUtils/apiMiddleware');
+
+const runE2EWithRetry = id => runE2E(id).catch(() => runE2E(id));
+/*
+ * Automated migration for gcloud deployments
+ * https://cloud.google.com/appengine/docs/admin-api/getting-started/
+ * https://cloud.google.com/appengine/docs/admin-api/reference/rest/v1/apps.services.versions
+ * https://cloud.google.com/appengine/docs/admin-api/deploying-apps
+ * https://cloud.google.com/appengine/docs/admin-api/creating-config-files
+ */
+
+const { CI_BUILD_NUMBER } = process.env;
+
+module.exports = {
+	command: 'create',
+	describe: 'deploy the current application to production',
+	builder: yargs =>
+		yargs.options({
+			version: {
+				alias: 'v',
+				default: CI_BUILD_NUMBER,
+				demandOption: true,
+				describe: 'The version ID to deploy',
+			},
+			incrementWait: {
+				default: 60000, // 1 minute
+				describe: 'The delay between migration increments',
+			},
+			incrementPercentage: {
+				default: 100,
+				describe:
+					'The percentage of traffic to migrate in each increment',
+			},
+			maxInstances: {
+				default: 196,
+				describe:
+					'The maximum number of available instances in a GAE deployment',
+			},
+			deployCount: {
+				default: 1,
+				describe: 'The number of parallel versions to deploy',
+			},
+			targetTraffic: {
+				default: 100,
+				describe:
+					'The total amount of traffic to be migrated to the deployment',
+			},
+			pollWait: {
+				default: 10000, // 10 seconds
+				describe: 'The time to wait between progress checks',
+			},
+			noCanary: {
+				describe: 'Disable canary test',
+			},
+		}),
+	middlewares: [apiMiddleware],
+	handler: argv =>
+		argv.getDeployApi().then(({ deploy, versions, migrate }) =>
+			versions.validate
+				.sufficientQuota()
+				.then(deploy.create)
+				.then(
+					() =>
+						argv.noCanary ||
+						Promise.all(argv.versionIds.map(runE2EWithRetry))
+				)
+				.catch(error => {
+					console.log(chalk.red(`Stopping deployment: ${error}`));
+					console.log(chalk.red('Cleaning up failed deployment...'));
+					// clean up deployed versions and exit
+					return deploy.del().then(() => process.exit(1));
+				})
+				.then(() =>
+					migrate().catch(error => {
+						console.log(chalk.red(`Migration stopped: ${error}`));
+						if (
+							!(error instanceof versions.validate.RedundantError)
+						) {
+							// no need to return non-zero exit code for newer deployment
+							process.exit(1);
+						}
+					})
+				)
+		),
+};
