93e2774e165152526f2e892cb89b45ccf3598c76
Michael McGahan <mike.mcgahan@gmail.com>
2018-03-06 11:14:19 +1300

    more documentation, bail if no versions serving traffic

--- src/commands/deployCommands/clean.js
+++ src/commands/deployCommands/clean.js
@@ -1,9 +1,25 @@
 const apiMiddleware = require('../deployUtils/apiMiddleware');
 
+const sortByStartTime = versions =>
+	versions.sort(({ createTime: a }, { createTime: b }) => {
+		if (a > b) return -1;
+		if (a < b) return 1;
+		return 0;
+	});
+
+/*
+ * This function will stop 'old' deployments, leaving a specified minimum number
+ * still 'warm' and available to serve production traffic. It is 'safe', in that
+ * it attempts to guarantee not to stop any version that is currently serving
+ * traffic. In addition, it guarantees not to stop any version _newer_ than the
+ * oldest version currently allocated production traffic.
+ */
 const stopOldVersions = (serving, api, versions) =>
+	// find out which versions are currently serving traffic (allocations)
 	api
 		.allocations()
 		.then(allocs => {
+			// only interested in 'warm' versions - ignore previously-stopped versions
 			const runningVersions = versions.filter(
 				v => v.servingStatus === 'SERVING'
 			);
@@ -13,12 +29,19 @@ const stopOldVersions = (serving, api, versions) =>
 				'Currently available to serve:',
 				runningVersions.map(({ id }) => id).join(', ')
 			);
+			// find the oldest version that is currently serving prod traffic
 			const oldestAllocIndex = runningVersions.reduce(
 				(oldestI, v, i) => (servingIds.includes(v.id) ? i : oldestI),
 				-1
 			);
+			if (oldestAllocIndex === -1) {
+				throw new Error(
+					'no versions currently serving traffic - check status and retry'
+				);
+			}
 
-			const stopIndex = Math.max(oldestAllocIndex + serving, 0);
+			// only stop versions that are _older_ than oldest currently-serving version
+			const stopIndex = oldestAllocIndex + serving;
 			const versionsToStop = runningVersions.slice(stopIndex);
 			console.log(
 				'Stopping version(s):',
@@ -56,23 +79,19 @@ module.exports = {
 		return argv
 			.getDeployApi()
 			.then(api =>
-				api.versions.get().then(versions => {
-					const sortedVersions = versions.sort(
-						({ createTime: a }, { createTime: b }) => {
-							if (a > b) return -1;
-							if (a < b) return 1;
-							return 0;
-						}
-					);
-					return Promise.all([
-						stopOldVersions(argv.serving, api, sortedVersions),
-						deleteOldestVersions(
-							argv.available,
-							api,
-							sortedVersions
-						),
-					]);
-				})
+				api.versions
+					.get()
+					.then(sortByStartTime)
+					.then(sortedVersions =>
+						Promise.all([
+							stopOldVersions(argv.serving, api, sortedVersions),
+							deleteOldestVersions(
+								argv.available,
+								api,
+								sortedVersions
+							),
+						])
+					)
 			)
 			.catch(err => console.error(err));
 	},
