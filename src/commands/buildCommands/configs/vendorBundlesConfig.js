e41b13162f86c22b3afba8a1d989b9ea92f368a4
Sadaf Shahsahebi <sadaf@meetup.com>
2018-04-24 12:56:12 -0400

    prod plugins

--- src/commands/buildCommands/configs/vendorBundlesConfig.js
+++ src/commands/buildCommands/configs/vendorBundlesConfig.js
@@ -1,53 +1,52 @@
 const ManifestPlugin = require('webpack-manifest-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const webpack = require('webpack');
 const path = require('path');
 
 const paths = require('../paths');
 const env = require('../env');
-// const prodPlugins = require('./prodPlugins');
+const prodPlugins = require('./prodPlugins');
 
-const dllName = '[name]_lib';
+const dllName = "[name]_lib";
 
 const config = {
-	mode: env.properties.isProd ? 'production' : 'development',
+	mode: env.properties.isProd ? "production" : "development",
 	entry: {
 		react: [
-			'react',
-			'react-dom',
-			'react-facebook-login',
-			'react-helmet',
-			'react-intl',
-			'react-redux',
-			'react-router',
-			'react-router-dom',
-			'react-waypoint',
+			"react",
+			"react-dom",
+			"react-facebook-login",
+			"react-helmet",
+			"react-intl",
+			"react-redux",
+			"react-router",
+			"react-router-dom",
+			"react-waypoint"
 		],
 		vendor: [
-			'autosize',
-			'consolidated-events',
-			'fbjs',
-			'flatpickr',
-			'history',
-			'intl-messageformat',
-			'intl-relativeformat',
-			'prop-types',
-			'qs',
-			'redux',
-			'rison',
+			"autosize",
+			"consolidated-events",
+			"fbjs",
+			"flatpickr",
+			"history",
+			"intl-messageformat",
+			"intl-relativeformat",
+			"prop-types",
+			"qs",
+			"redux",
+			"rison"
 		],
 	},
 	output: {
 		path: paths.output.browser,
-		filename: '[name].[chunkhash].js',
+		filename: "[name].[chunkhash].js",
 		hashDigestLength: 8,
-		// The name of the global variable which the library's
-		// require() function will be assigned to
 		library: dllName,
 	},
 
 	plugins: [
 		new webpack.EnvironmentPlugin({
-			NODE_ENV: 'development', // required for prod build of React
+			NODE_ENV: "development", // required for prod build of React
 		}),
 		new webpack.DllPlugin({
 			// The path to the manifest file which maps between
@@ -61,12 +60,29 @@ const config = {
 		}),
 		new ManifestPlugin({
 			publicPath: `${env.properties.asset_server.path}/`,
-		}),
+		})
 	],
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({
+				uglifyOptions: {
+					compress: {
+						warnings: false,
+					},
+					output: {
+						comments: false,
+					},
+				},
+				sourceMap: true,
+			})
+		],
+	},
 };
+
 
 if (env.properties.isProd) {
 	config.plugins = config.plugins.concat(prodPlugins);
 }
+
 
 module.exports = config;
