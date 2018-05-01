179099f5b34f629ca897cbb3190d1431b6b62667
Sadaf Shahsahebi <sadaf@meetup.com>
2018-05-01 11:07:32 -0400

    fix start-dev scripts

--- src/commands/run.js
+++ src/commands/run.js
@@ -1,5 +1,4 @@
 const chalk = require('chalk');
-
 const startDev = require('./runScripts/start-dev');
 
 module.exports = {
@@ -9,7 +8,10 @@ module.exports = {
 	handler: argv => {
 		const { NODE_ENV } = process.env;
 		const envString = NODE_ENV || 'development';
-		console.log(chalk.green(`NODE_ENV=${envString}`));
+
+		console.log(chalk.gray('$ mope run'));
+		console.log(chalk.gray(`NODE_ENV=${envString}`));
+
 		if (envString !== 'development') {
 			console.log(
 				chalk.red(`Cannot run dev server with NODE_ENV=${envString}`)
@@ -17,13 +19,14 @@ module.exports = {
 			console.log(chalk.red(`Set NODE_ENV=development`));
 			process.exit(1);
 		}
+
 		if (argv['log-static']) {
 			process.env.DEBUG = 'express:router';
 		}
-		// TODO: check for prerequisites (e.g. up-to-date build/... files)
+
 		// execute the start-dev script
-		console.log(chalk.blue('Preparing the dev app server...'));
 		startDev();
+
 		return;
 	},
 };
