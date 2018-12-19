a71dcd7d397174b506cf685b6d0ba2a086ba072a
Michael McGahan <mike.mcgahan@gmail.com>
2018-12-20 10:08:37 +1300

    remove TODO comment, document the 'stop' command

--- src/commands/deployCommands/stop.js
+++ src/commands/deployCommands/stop.js
@@ -2,6 +2,15 @@ const apiMiddleware = require('../deployUtils/apiMiddleware');
 
 const { CI_BUILD_NUMBER } = process.env;
 
+/**
+ * This command will make a simple attempt to STOP the specified version. If
+ * another operation is already being performed on the version (e.g. it is still
+ * in the process of deploying), it will fail with an error message but will not
+ * return a non-zero exit code.
+ *
+ * Use this command to free up the instance resources being consumed by a deployment
+ * that has been set to SERVING status
+ */
 module.exports = {
 	command: 'stop',
 	description: 'stop the specified version',
@@ -14,11 +23,9 @@ module.exports = {
 			},
 		}),
 	middlewares: [apiMiddleware],
-	handler: argv => {
-		return argv
+	handler: argv =>
+		argv
 			.getDeployApi()
 			.then(api => api.versions.stop(argv.versionIds.map(id => ({ id }))))
-			.catch(err => console.error(err));
-		// should we wait to retry if version is currently still deploying?
-	},
+			.catch(err => console.error(err)),
 };
