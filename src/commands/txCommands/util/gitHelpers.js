96189b9281ad838eea52d102810236b867193051
Michael McGahan <mike.mcgahan@gmail.com>
2019-02-19 10:35:02 +1300

    can't defer

--- src/commands/txCommands/util/gitHelpers.js
+++ src/commands/txCommands/util/gitHelpers.js
@@ -10,9 +10,7 @@ require('rxjs/add/observable/if');
 require('rxjs/add/observable/of');
 require('rxjs/add/observable/empty');
 require('rxjs/add/observable/defer');
-const child_process$ = Rx.Observable.defer(() =>
-	Rx.Observable.bindNodeCallback(child_process.exec)
-);
+const child_process$ = Rx.Observable.bindNodeCallback(child_process.exec);
 
 /**
  * Creates git commit of what has been staged
